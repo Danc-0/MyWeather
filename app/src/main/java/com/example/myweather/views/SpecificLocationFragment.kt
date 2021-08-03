79e7cbac6a3e637d0227dad81d52a72ac8ee811d
Danc-0 <lunjaludaniel@gmail.com>
2021-08-03 06:55:46 +0300

    Designs

---
+++ app/src/main/java/com/example/myweather/views/SpecificLocationFragment.kt
@@ -0,0 +1,16 @@
+package com.example.myweather.views
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myweather.R
+
+class SpecificLocationFragment : Fragment(R.layout.fragment_specific_location) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
